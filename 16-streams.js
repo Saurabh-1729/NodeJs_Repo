08b5a0a66471fdaf3150042ba804ff941131233c
Saurabh-1729 <ss1013188@gmail.com>
2023-08-13 19:38:27 +0530

    fourth commit

--- 16-streams.js
+++ 16-streams.js
@@ -7,7 +7,10 @@ const { createReadStream } = require('fs')
 // const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
 const stream = createReadStream('./content/big.txt')
 
+
 stream.on('data', (result) => {
   console.log(result)
 })
+
 stream.on('error', (err) => console.log(err))
+
